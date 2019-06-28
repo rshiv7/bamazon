a12039ef48d958add101866be876f78241cf6c99
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-27 19:23:09 -0700

    Created database and connection

---
+++ Bamazon.sql
@@ -0,0 +1,13 @@
+-- Create a database called 'Bamazon' and switch into it for this activity --
+CREATE DATABASE Bamazon;
+USE Bamazon;
+
+-- Create a table called 'products' which will contain the store inventory --
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
