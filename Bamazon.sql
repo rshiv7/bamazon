e1a83715ec33e7a683fabc09d199482866836ba2
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-27 22:16:46 -0700

    Added products

--- Bamazon.sql
+++ Bamazon.sql
@@ -1,8 +1,8 @@
--- Create a database called 'Bamazon' and switch into it for this activity --
+-- Create a database called "Bamazon" and switch into it for this activity --
 CREATE DATABASE Bamazon;
 USE Bamazon;
 
--- Create a table called 'products' which will contain the store inventory --
+-- Create a table called "products" which will contain the store inventory --
 CREATE TABLE products (
 	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	product_name VARCHAR(30) NOT NULL,
@@ -11,3 +11,17 @@ CREATE TABLE products (
 	stock_quantity INTEGER(11) NOT NULL,
 	PRIMARY KEY (item_id)
 );
+-- Insert data into the "products" table --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Dove Shampoo', 'Cosmetics', 5.75, 100),
+		('Dove Conditioner', 'Cosmetics', 6.25, 100),
+		('Glad 12 Gal Trash Bags', 'Grocery', 5.99, 100),
+		('Brawny Paper Towels', 'Grocery', 4.25, 100),
+		('Granny Smith Apples', 'Produce', 0.35, 100),
+		('Chiquita Bannana', 'Produce', 0.20, 100),
+		('Tropicana Orange Juice', 'Grocery', 4.45, 100),
+		('Horizon Organic Milk', 'Grocery', 4.50, 100),
+		('Huggies Diapers', 'Children', 2.75, 100),
+		('Charmin Toiler Paper', 'Grocery', 12.99, 100),
+		('Pampers Baby Wipes', 'Children', 1.50, 100),
+		('Ben & Jerry Ice Cream', 'Grocery', 3.25, 100);
